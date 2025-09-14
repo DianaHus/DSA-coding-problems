62ccfcc2b2652becd3ba069cc21643c2dbf5a011
Diana Husanu <dhusanu0@gmail.com>
2025-09-14 22:57:04 +0200

    reverse linked list II leetcode 92

--- lists.py
+++ lists.py
@@ -31,5 +31,35 @@ for i in range(2, 11):
     current = current.next
 
 #node1.print_list()
-rev = reverse(head)
-rev.print_list()
+#rev = reverse(head)
+#rev.print_list()
+
+"""
+Leetcode 92. Reverse Linked List II
+Given the head of a singly linked list and two integers left and right 
+where left <= right, reverse the nodes of the list from position left 
+to position right, and return the reversed list.
+"""
+
+def reverseBetween(head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+    if not head or left == right:
+        return head
+
+    dummy = ListNode(0)
+    dummy.next = head
+    prev = dummy
+
+    for _ in range(left - 1):
+        prev = prev.next
+
+    curr = prev.next
+    for _ in range(right - left):
+        temp = curr.next
+        curr.next = temp.next
+        temp.next = prev.next
+        prev.next = temp
+
+    return dummy.next
+
+rev = reverseBetween(head, 2, 8)
+head.print_list()
